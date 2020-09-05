c840c8748b4281a1618871a81bf8468d601b83be
Cássio Paixão <cassiopaixaoti@gmail.com>
2020-09-05 14:54:20 -0300

    chore: create routes create, update and remove in CoffeesController

--- src/coffees/coffee.controller.ts
+++ src/coffees/coffee.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Res, Patch, Delete } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -10,5 +10,21 @@ export class CoffeesController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return `This action returns #${id} coffee`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
+    // return `This action creates a coffee`;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This action updates #${id} coffee`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes #${id} coffee`;
   }
 }

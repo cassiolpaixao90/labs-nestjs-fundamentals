514e16a0efe3da6fac9029cda1fcf3505090fc85
Cássio Paixão <cassiopaixaoti@gmail.com>
2020-09-07 19:11:40 -0300

    chore: create custom param decorators

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -3,6 +3,7 @@ import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { Public } from 'src/common/decorators/public.decorator';
 import { CoffeesService } from './coffees.service';
 import { ParseIntPipe } from 'src/common/pipes/parse-int.pipe';
+import { Protocol } from 'src/common/decorators/protocol.decorator';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -11,7 +12,8 @@ export class CoffeesController {
 
   @Public()
   @Get()
-  findAll(@Query() paginationQuery: PaginationQueryDto) {
+  findAll(@Protocol('https') protocol: string, @Query() paginationQuery: PaginationQueryDto) {
+    console.log('protocol', protocol);    
     return this.coffeesService.findAll(paginationQuery);
   }
 

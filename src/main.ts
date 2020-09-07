c75c7df42722f95dda6c7dc6b00b8d138f1e8129
Cássio Paixão <cassiopaixaoti@gmail.com>
2020-09-07 19:20:06 -0300

    chore: add config swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from 'src/app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { WrapResponseInterceptor } from './common/interceptors/wrap-response.interceptor';
 import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
@@ -22,6 +24,17 @@ async function bootstrap() {
     new WrapResponseInterceptor(), 
     new TimeoutInterceptor()
   );
+
+  const options = new DocumentBuilder()
+    .setTitle('Iluvcoffee')
+    .setDescription('Coffee application')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
